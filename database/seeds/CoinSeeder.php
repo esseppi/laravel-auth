1d3250a1ab6163a3a4475755e17b12236ceedad0
Your Name <you@example.com>
2022-05-20 15:00:22 +0200

    new file

--- database/seeds/CoinSeeder.php
+++ database/seeds/CoinSeeder.php
@@ -15,14 +15,14 @@ class CoinSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i = 0; $i < 100; $i++) {
-            $slug = $faker->words(rand(2, 4), true);
+            $name = $faker->lastName();
             Coin::create([
-                'name'          => $faker->lastName(),
+                'name'          => $name,
                 'thumb'         => $faker->imageUrl(640, 480, 'animals', true),
                 'description'   => $faker->sentence(rand(0, 10)),
                 'price'         => $faker->numberBetween(0, 10000),
                 'amount'        => $faker->numberBetween(0, 1000),
-                'slug'          => Coin::generateSlug($slug)
+                'slug'          => Coin::generateSlug($name)
             ]);
         }
     }
